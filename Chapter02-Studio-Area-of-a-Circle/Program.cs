c73a0113170fbd6bce62c9700d1ea659ed01ddb6
Gebbia <gebbianatasha@gmail.com>
2022-08-07 12:05:24 -0500

    first commit34

--- Chapter02-Studio-Area-of-a-Circle/Program.cs
+++ Chapter02-Studio-Area-of-a-Circle/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+class Circle
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+        double diameter = 2 * radius;
+        Console.WriteLine("Area of circle: " + area + "\nCircumference of circle: " + circumference + "\nDiameter of circle: " + diameter);
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
